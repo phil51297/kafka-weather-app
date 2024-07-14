3746cf4694bce36b413a53ab06f3e37a09992ed8
eliotmeurillon <eliot.meurillon@ooeo.fr>
2024-07-14 17:49:44 +0200

    chore: Update weatherDataDiv rendering in app.js

--- front/app.js
+++ front/app.js
@@ -5,8 +5,26 @@ htmx.on("#weatherData", "htmx:afterSettle", function (event) {
     weatherDataDiv.innerHTML = "";
     data.forEach((weather) => {
       const weatherDiv = document.createElement("div");
-      weatherDiv.innerText = JSON.stringify(weather, null, 2);
-      weatherDataDiv.appendChild(weatherDiv);
+
+      const city = document.createElement("h3");
+      city.innerText = weather.city;
+      weatherDiv.appendChild(city);
+
+      const temperature = document.createElement("p");
+      temperature.innerText = `Temperature: ${weather.temperature} °C`;
+      weatherDiv.appendChild(temperature);
+
+      const description = document.createElement("p");
+      description.innerText = `Description: ${weather.description}`;
+      weatherDiv.appendChild(description);
+
+      const epochTime = document.createElement("p");
+      epochTime.innerText = `Epoch Time: ${weather.epoch_time}`;
+      weatherDiv.appendChild(epochTime);
+
+      const hasPrecipitation = document.createElement("p");
+      hasPrecipitation.innerText = `Has Precipitation: ${weather.has_precipitation}`;
+      weatherDiv.appendChild(hasPrecipitation);
     });
   } catch (error) {
     console.error("Error parsing weather data:", error);
